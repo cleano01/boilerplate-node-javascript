4028980c97dd1a52c1f4543ab50e61cca302b4f0
cleano01 <cleano.ferreira@dce.ufpb.br>
2022-11-02 21:56:20 -0300

    boilerplate/feature added .env to SERVICE_NAME (api name)

--- src/common/utils/logger.js
+++ src/common/utils/logger.js
@@ -3,7 +3,7 @@ import { createLogger, format, transports } from "winston";
 const logger = createLogger({
   level: "info",
   format: format.combine(format.timestamp(), format.json()),
-  defaultMeta: { service: "boilerplate-node-js" },
+  defaultMeta: { service: process.env.SERVICE_NAME },
   exitOnError: false,
   transports: [new transports.Console()],
 });
