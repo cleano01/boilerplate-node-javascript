68b94da440d2bab5888da8775765e4b6d9b94718
cleano01 <cleano.ferreira@dce.ufpb.br>
2022-11-02 21:51:44 -0300

    boilerplate/feature added default metadata to show service specific

--- src/common/utils/logger.js
+++ src/common/utils/logger.js
@@ -3,6 +3,7 @@ import { createLogger, format, transports } from "winston";
 const logger = createLogger({
   level: "info",
   format: format.combine(format.timestamp(), format.json()),
+  defaultMeta: { service: "boilerplate-node-js" },
   exitOnError: false,
   transports: [new transports.Console()],
 });
